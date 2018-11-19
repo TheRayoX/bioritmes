a6bc8017b4ca94ef9802014d62444610d0bc1074
Albert <therayo12@gmail.com>
2018-11-19 21:10:43 +0100

    Modificacion views
    Añadidas dos fuentes con su respectivo CDN y modificación del navbar.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,30 +8,39 @@
 
         <!-- BootStrap -->
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC:700" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="{{ asset('js/bootstrap.min.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
     </head>
-    <body>
+    <body style="background-color:#dcdcdc">
         <div class="flex-center position-ref full-height">
-        	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
-			  <a class="navbar-brand text-light" href="#">home </a>
+        	<nav class="navbar navbar-expand-sm" style="background-color:#b22222; font-family:'Amatic SC'; font-size:25px;">
+			  <a class="navbar-brand text-light" href="index.php">LOGO</a>
 			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			    <span class="navbar-toggler-icon"></span>
-			 	 </button>
-			  	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			 	</button>
+			  	<div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
 			    <div class="navbar-nav">
-			      <a class="nav-item nav-link text-light" href="#">index</a>
-			      <a class="nav-item nav-link text-light" href="#">pagina1</a>
-			      <a class="nav-item nav-link text-light" href="#">pagina2</a>
+			      <a class="nav-item nav-link text-light border border-dark" href="index.php">Inicio</a>
+			      <a class="nav-item nav-link text-light border border-dark" href="#">INICIO</a>
+            <a class="nav-link nav-item disabled border border-dark" href="#">Resultado</a>
 			    </div>
 			  </div>
 			</nav>
+        <div class="container-fluid" style="height:100%;">
+            <div class="row">
             <div class="container">
                     @yield('content')
             </div>
-            <div class="container-fluid bg-dark" style="position:fixed;bottom:0px;">
+
+            </div>
+
+          </div>
+
+            <div class="container-fluid" style="position:fixed;bottom:0px; background-color:#b22222;">
     			<div class="row">
     				<div class="col-8 offset-2">
     					<p class="text-light text-center mt-4 small">Terms of Use | Privacy Policy | Mobile site<br/>2018 Copyright By Viox and TheRayoX<br/>All Rights Reserved</p>
