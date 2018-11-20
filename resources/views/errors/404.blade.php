2651844af54c7b37aa147b41bcb0a10d67e8c07f
Albert <therayo12@gmail.com>
2018-11-20 21:10:27 +0100

    cambios 1.5.1
    Cambios:
    - Modificar página 'error404'.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,10 +2,16 @@
 @section('title')
     Error 404
 @stop
-@section('imagen')
-    <img src="https://cdn.pixabay.com/photo/2018/11/01/05/33/british-columbia-3787200_1280.jpg">
-@stop
 @section('contenido')
-    <div class="container">
+    <div class="container pl-5">
+        <div class="row">
+            <div class="col-7 offset-5 text-right">
+                <h3 style="font-family:'Monoton'; margin-top:6em; font-size:48px;">ERROR 404</h3>
+                <p>Página no encontrada</p>
+                <div class="text-center" style="margin-left:50%;">
+                    <button type="button" class="btn btn-outline-secondary">Inicio</button>
+                </div>
+            </div>
+        </div>
     </div>
 @stop
