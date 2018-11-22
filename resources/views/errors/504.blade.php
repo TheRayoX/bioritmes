881f2af8027e97e435c8d4d756448591ef96fd98
Home <homexiang3@gmail.com>
2018-11-22 19:38:45 +0100

    1.9.1
    finiquitado tema de errores

--- resources/views/errors/504.blade.php
+++ resources/views/errors/504.blade.php
@@ -8,7 +8,7 @@
     <div class="container pl-5">
         <div class="row">
             <div class="col-12 text-center" style="color:white;">
-                <h3 style="font-family:'Monoton'; margin-top:5.5em; font-size:48px;">ERROR 504</h3>
+                <h3 style="font-family:'Monoton';color:white; margin-top:5.5em; font-size:48px;">ERROR 504</h3>
                 <p>Gateway Timeout</p>
                 <div class="text-center">
                     <a href="/"><button type="button" class="btn btn-outline-light">Recargar</button></a>
