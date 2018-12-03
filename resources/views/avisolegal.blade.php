4059e3986202f45d9601427f74a335afaa73b839
Albert <therayo12@gmail.com>
2018-12-03 19:20:36 +0100

    1.20.3

--- resources/views/avisolegal.blade.php
+++ resources/views/avisolegal.blade.php
@@ -5,10 +5,10 @@
     <h3 class="ml-5" style="font-weight:bold;">1.- Objeto </h3>
         <p>
             Estas condiciones generales (en adelante las "Condiciones Generales") 
-            regulan el uso de todos los servicios del Portal www.WebGranada.com 
+            regulan el uso de todos los servicios del Portal Dual.test 
             (en adelante el "Portal"). Por la mera utilización del Portal el usuario 
             expresa la plena aceptación y sin reservas de las mismas, las cuales podrán 
-            ser modificadas por WebGranada.com en cualquier momento. Los usuarios se 
+            ser modificadas por Dual.test en cualquier momento. Los usuarios se 
             ven asimismo, sometidos a todas aquellas condiciones particulares, avisos 
             o reglamentos de instrucciones que se pongan en su conocimiento en relación 
             con contenidos o servicios concretos, que completen lo previsto en estas Condiciones Generales.
@@ -16,44 +16,44 @@
     <h3 class="ml-5" style="font-weight:bold;">2.- Carácter Gratuito de los Servicios</h3>
         <p>
             Los servicios ofrecidos por el Portal son los que en cada momento sean ofertados 
-            en la red WebGranada.com, siendo todos ellos, salvo que expresamente se indique 
+            en la red Dual.test, siendo todos ellos, salvo que expresamente se indique 
             lo contrario, de carácter gratuito. La utilización de algunos de los servicios 
             requiere el previo registro del usuario.
         </p>
     <h3 class="ml-5" style="font-weight:bold;">3.- Exclusión de responsabilidad</h3>
     <h5 class="ml-3" style="font-weight:bold;">3.1.- Por el funcionamiento del Portal y sus servicios</h5>
         <p>
-            WebGranada.com no garantiza la disponibilidad y continuidad del funcionamiento 
+            Dual.test no garantiza la disponibilidad y continuidad del funcionamiento 
             del Portal, de sus servicios y sus contenidos. Si fuera razonablemente posible, 
-            WebGranada.com avisará previamente de las interrupciones en el funcionamiento 
+            Dual.test avisará previamente de las interrupciones en el funcionamiento 
             de los mismos. 
         </p>
         <p>
-            WebGranada.com no es responsable por los daños o perjuicios de cualquier naturaleza 
+            Dual.test no es responsable por los daños o perjuicios de cualquier naturaleza 
             que puedan deberse a la falta de disponibilidad o de continuidad del Portal o de cualquiera 
             de sus servicios, o por los fallos en el acceso a las distintas páginas web del Portal o 
             aquellas desde las que se prestan determinados servicios.
         </p>
     <h5 class="ml-3" style="font-weight:bold;">3.2.- Por la utilización del Portal y los servicios por los usuarios.</h5>
         <p>
-            WebGranada.com no tiene obligación y no controla la utilización que del Portal y sus 
+            Dual.test no tiene obligación y no controla la utilización que del Portal y sus 
             servicios hacen los usuarios y por ello no garantiza que el uso por parte de los mismos 
             se realice conforme a la ley, las presentes Condiciones Generales y en su caso, cualquier 
-            otra condición particular que se establezca. WebGranada.com no es responsable de los 
+            otra condición particular que se establezca. Dual.test no es responsable de los 
             daños o perjuicios de cualquier naturaleza que pudieran deberse a la incorrecta, 
             inapropiada o inadecuada utilización que los usuarios realicen del Portal o sus servicios.
         </p>
     <h5 class="ml-3" style="font-weight:bold;">3.3.- De los contenidos propios</h5>
         <p>
             Los contenidos de todo tipo incluidos en las páginas web del Portal son facilitados 
-            de buena fe por WebGranada.com con información procedente tanto de fuentes internas 
+            de buena fe por Dual.test con información procedente tanto de fuentes internas 
             (elaboración propia) como externas a la propia web. En razón de esta circunstancia y 
-            de la gran cantidad de información accesible, WebGranada.com no garantiza la total 
+            de la gran cantidad de información accesible, Dual.test no garantiza la total 
             exactitud o actualización de los datos o textos disponibles a pesar de que emplea 
             sus mejores esfuerzos para que así sea.
         </p>
         <p>
-            WebGranada.com no garantiza la utilidad del Portal o de sus servicios para ninguna 
+            Dual.test no garantiza la utilidad del Portal o de sus servicios para ninguna 
             actividad en particular o la idoneidad de los contenidos incluidos en las páginas web 
             del mismo para fines particulares, por lo que tanto el acceso a las páginas, como el 
             uso de la información, contenidos y servicios se realiza bajo la única y exclusiva 
@@ -61,7 +61,7 @@
         </p>
     <h5 class="ml-3" style="font-weight:bold;">3.4.- De los contenidos de Terceros</h5>
         <p>
-            WebGranada.com no es en ningún caso responsable, ni de forma directa ni subsidiaria, 
+            Dual.test no es en ningún caso responsable, ni de forma directa ni subsidiaria, 
             por ningún contenido, información, comunicación, opinión o manifestación de cualquier 
             tipo que tenga su origen en el usuario o cualquier tercera persona o entidad y que se 
             comunique, difunda, transmita o exhiba a través de los servicios del Portal.
@@ -70,7 +70,7 @@
         <p>
             El Portal pone a disposición de los usuarios dispositivos técnicos de enlaces, ya sean 
             links, directorios o herramientas de búsqueda, los cuales permiten el acceso a páginas 
-            web o sites que son gestionados por terceros. WebGranada.com no ejerce ninguna titularidad 
+            web o sites que son gestionados por terceros. Dual.test no ejerce ninguna titularidad 
             sobre los mismos, y no comercializa ni ofrece los contenidos y servicios que contienen, 
             por lo tanto no asume ninguna responsabilidad ni directa ni subsidiaria sobre dichas páginas 
             web o sites.
@@ -85,7 +85,7 @@
         </p>
         <p>
             Asimismo, y salvo que haya obtenido previa y expresa autorización específicamente otorgada 
-            al efecto por parte de WebGranada.com, el usuario se abstendrá de obtener o intentar obtener 
+            al efecto por parte de Dual.test, el usuario se abstendrá de obtener o intentar obtener 
             cualquier tipo de contenidos, ya sean textos, gráficos, dibujos, archivos de sonido, imágenes 
             o fotografías, vídeos, software y en general cualquier clase de material accesible a través 
             del Portal o los servicios, empleando para ello medios distintos de los que se hayan puesto 
@@ -95,19 +95,19 @@
         <p>
             Todos los contenidos así como nombres, logos, marcas y bases de datos accesibles en 
             las páginas web del Portal están sujetos a derechos de propiedad industrial e intelectual 
-            de WebGranada.com o de terceros titulares de los mismos. En ningún caso el acceso a 
+            de Dual.test o de terceros titulares de los mismos. En ningún caso el acceso a 
             las páginas web del Portal implica ningún tipo de renuncia, transmisión o cesión total 
             o parcial de los mencionados derechos, ni confiere ningún derecho de utilización, alteración, 
             explotación, reproducción, distribución o comunicación pública sobre los contenidos o bienes 
             de propiedad industrial sin la previa y expresa autorización específicamente otorgada a tal 
-            efecto por parte de WebGranada.com o del tercero titular de los derechos, salvo los derechos 
+            efecto por parte de Dual.test o del tercero titular de los derechos, salvo los derechos 
             de visualizar y de realizar copias para uso personal y exclusivo del usuario, los cuales deberán 
             ejercerse siempre conforme a los principios de la buena fe y la legislación aplicable.
         </p>
     <h3 class="ml-5" style="font-weight:bold;">6.- Protección de Datos</h3>
         <p>
             Mediante el presente Aviso Legal y de acuerdo con lo establecido en la Ley Orgánica 15/1999, 
-            de Protección de Datos de Carácter Personal, WebGranada.com avisa a los usuarios de su política 
+            de Protección de Datos de Carácter Personal, Dual.test avisa a los usuarios de su política 
             de protección de datos, para que estos determinen libre y voluntariamente si desean facilitar 
             los datos personales que se le puedan requerir con ocasión de la suscripción o alta en algunos 
             de los servicios ofrecidos. Salvo en los campos en que se indique lo contrario, las respuestas 
@@ -115,14 +115,14 @@
         </p>
         <p>
             Los datos personales recogidos serán tratados de forma secreta y confidencial y su recogida 
-            tiene como finalidad la gestión y prestación de los servicios ofrecidos por WebGranada.com 
+            tiene como finalidad la gestión y prestación de los servicios ofrecidos por Dual.test 
             en el Portal en cada momento. El usuario acepta que los datos personales por él facilitados 
             puedan ser objeto de tratamiento en ficheros automatizados titularidad y responsabilidad de 
-            WebGranada.com.  
+            Dual.test.  
         </p>
         <p>
-            Asimismo, el titular de los datos autoriza de manera expresa a WebGranada.com a fin de que 
-            los mismos puedan ser cedidos a empresas que formen parte de WebGranada.com para idénticas 
+            Asimismo, el titular de los datos autoriza de manera expresa a Dual.test a fin de que 
+            los mismos puedan ser cedidos a empresas que formen parte de Dual.test para idénticas 
             finalidades a las anteriormente indicadas.  
         </p>
         <p>
@@ -130,14 +130,14 @@
             carácter revocable, sin efectos retroactivos. Asimismo, y conforme a la mencionada ley, 
             sobre los datos personales los usuarios podrán ejercer los derechos de acceso, rectificación 
             y en su caso cancelación, para lo cual deberán enviar una solicitud por correo certificado 
-            con acuse de recibo dirigida a WebGranada.com, incluyendo: (I) nombre, apellidos y fotocopia 
+            con acuse de recibo dirigida a Dual.test, incluyendo: (I) nombre, apellidos y fotocopia 
             del documento oficial de identificación del interesado y en su caso, el documento acreditativo 
             de su representación; (II) la petición razonada en que se concreta la solicitud; (III) domicilio 
             a efectos de notificaciones y (IV) fecha y firma del solicitante.
         </p>
     <h3 class="ml-5" style="font-weight:bold;">7.- Utilización de cookies</h3>
         <p>
-            WebGranada.com utiliza cookies cuando un usuario navega por los sites y páginas web del 
+            Dual.test utiliza cookies cuando un usuario navega por los sites y páginas web del 
             Portal. La finalidad en la utilización de las mismas es identificar al usuario, 
             seguir su trayectoria durante las visitas y conocerlo mejor para agilizar el servicio 
             ofrecido. Las cookies, se asocian únicamente a un usuario anónimo y no permiten acceder 
@@ -155,7 +155,7 @@
         </p>
         <p>
             Para cualquier controversia que pudiera derivarse de la aplicación de los 
-            servicios o interpretación o aplicación de las Condiciones Generales, WebGranada.com 
+            servicios o interpretación o aplicación de las Condiciones Generales, Dual.test 
             y el usuario, con renuncia expresa a su fuero propio se someten al de los Juzgados y 
             Tribunales de Granada (ESPAÑA).  
         </p>
