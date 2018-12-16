1b919e822eec47621efda3c283098f85e9e445e6
Albert <therayo12@gmail.com>
2018-12-16 15:56:10 +0100

    1.21

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,9 @@
 @extends('layouts.master')
 @section('content')
 <div class="container text-justify mt-3" style="font-size:14px;">
-<h3> Finalidad del Portal </h3>
-<p> 
-    Éste portal le permite calcular su biorritmo a partir de su fecha de nacimiento.
-</p>
-<h3> Creadores </h3>
-<h5> Home </h5>
-<p> Breve descripción </p>
-<h5> Albert </h5>
-<p> Breve descripción </p>
+<h5> Creadores </h5>
+<img src="">
+<h5> Finalidad del Portal </h5>
+<p></p>
 </div>
 @stop
